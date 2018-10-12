938e96e7bddeb6e5cdd722b34afe2005f64d8ba0
hopeaktian <hopeaktian@foxmail.com>
2018-10-12 22:33:12 +0800

    add info of order in index.

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@
 from datetime import datetime, timedelta
 from flask import Flask, redirect, url_for, render_template, session, g, jsonify, request
 from config import DevConfig
-from models import db, User
+from models import db, User, Order
 from controllers.printer import printer
 from controllers.login import login
 import datetime, json
@@ -22,7 +22,15 @@ app.add_template_filter(time_format, 'format_time')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if g.current_user != None:
+        user_order = Order.query.filter(Order.User_Id == g.current_user.Id).all()
+        lenth = len(user_order)
+        return render_template('index.html', user_order=user_order, lenth=lenth)
+    else:
+        user_order = None
+        lenth = 0
+        return render_template('index.html', user_order=user_order, lenth=lenth)
+
 
 @app.before_request
 def check_user():
@@ -42,4 +50,4 @@ app.register_blueprint(printer)
 app.register_blueprint(login)
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=80)
+    app.run(host='192.168.3.5', port=80)
