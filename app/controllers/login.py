2903077bc96400ab61fb2f3d5aedad4f19397766
hopeaktian <hopeaktian@gmail.com>
2018-12-28 23:22:37 +0800

    add findpassword page and change register style

--- app/controllers/login.py
+++ app/controllers/login.py
@@ -36,19 +36,63 @@ def register():
             error_msg = '请输入验证码'
         elif User.query.filter(User.Tel_Number == phone_number).first():
             error_msg = u'手机号已经注册过了'
-        # elif sms.verify(phone_number, code):
-        elif 1:
-            # user_forsql = User()
-            # user_forsql.Password = password
-            # user_forsql.Tel_Number = phone_number
-            # db.session.add(user_forsql)
-            # db.session.commit()
+        elif sms.verify(phone_number, code):
+            user_forsql = User()
+            user_forsql.Password = password
+            user_forsql.Tel_Number = phone_number
+            db.session.add(user_forsql)
+            db.session.commit()
             success = 1
+            # 自动登录
+            session['user_phone'] = phone_number
+            g.current_userphone = session['user_phone']
+            g.current_user = user_forsql
             return render_template('form.html', success=success)
         else:
             error_msg = u'验证码不正确，请检查！'
             return render_template('form.html', error_msg=error_msg)
     return render_template('form.html', error_msg=error_msg)
+
+@login.route("/findpassword", methods=['POST', 'GET'])
+def findpassword():
+    """
+    进行登录验证
+    :return:
+    """
+    error_msg = None
+    success = None
+    if request.method == 'GET':
+        # 获取 GET 请求参数
+        phone_number = request.args.get('mobile_phone_number')
+        if phone_number is not None:
+            if sms.send_message(phone_number):
+                return render_template('form.html', error_msg=error_msg)
+            else:
+                error_msg = 'Failed to get the verification code!'
+    elif request.method == 'POST':
+        phone_number = request.form['phone']
+        code = request.form['code']
+        newpassword = request.form['newpassword']
+
+        if code == '':
+            error_msg = '请输入验证码'
+        elif sms.verify(phone_number, code):
+            user_forsql = User.query.filter(User.Tel_Number == phone_number).first()
+            user_forsql.Password = newpassword
+            db.session.add(user_forsql)
+            db.session.commit()
+            success = 1
+            # 自动登录
+            session['user_phone'] = phone_number
+            g.current_userphone = session['user_phone']
+            g.current_user = user_forsql
+
+            return render_template('findpassword.html.', success=success)
+        else:
+            error_msg = u'验证码不正确，请检查！'
+            return render_template('findpassword.html', error_msg=error_msg)
+    return render_template('findpassword.html', error_msg=error_msg)
+
 
 @login.route("/clause", methods=['POST', 'GET'])
 def clause():
