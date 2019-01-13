583ba1f7353f96b6ee854cecf306329496ac1c6c
hopeaktian <hopeaktian@gmail.com>
2019-01-13 21:51:02 +0800

    update pay func

--- app/controllers/daoinkpay.py
+++ app/controllers/daoinkpay.py
@@ -7,7 +7,7 @@ author:    peak
 description:
 """
 from flask import Flask, Blueprint, render_template, request, flash, session, redirect, url_for, g
-import json
+import json, os
 from app.models import db, User, Order
 daoinkpay = Blueprint(
     'daoinkpay',
@@ -17,8 +17,14 @@ daoinkpay = Blueprint(
 from alipay import AliPay
 
 
-app_private_key_string = open("D:\Develop_Program\python\\rooprint\\app\controllers\private.pem").read()
-alipay_public_key_string = open("D:\Develop_Program\python\\rooprint\\app\controllers\public.pem").read()
+basepath = os.path.abspath(os.path.dirname(__file__))       # 当前文件所在目录
+parentdir = os.path.dirname(basepath)                       # 父级目录
+
+private = os.path.join(parentdir, "certs", "private.pem")            # 公钥路径
+public = os.path.join(parentdir, "certs", "public.pem")              # 私钥路径
+
+app_private_key_string = open(private).read()
+alipay_public_key_string = open(public).read()
 
 
 alipay = AliPay(
@@ -31,8 +37,10 @@ alipay = AliPay(
     debug=False          # 默认False
 )
 
+
 # 手机网站支付，需要跳转到https://openapi.alipay.com/gateway.do? + order_string
 
+# 支付宝支付跳转链接
 @daoinkpay.route('/pay', methods=['POST', 'GET'])
 def pay():
     cost = request.form.get("cost")
@@ -43,16 +51,17 @@ def pay():
         out_trade_no=tradeid,
         total_amount=cost,
         subject="道墨云印订单",
-        return_url="http://www.daoink.com/alipayresult",
+        return_url="http://www.daoink.com/alipayresult1",
         notify_url="" # 可选, 不填则使用默认notify url
     )
     url = "https://openapi.alipay.com/gateway.do?" + order_string
     return redirect(url)
 
-@daoinkpay.route('/alipayresult', methods=['GET', 'POST'])
-def alipayresult():
+# 支付宝同步回调接口
+@daoinkpay.route('/alipayresult1', methods=['GET', 'POST'])
+def alipayresult1():
     trade_out_id = request.args.get("out_trade_no")
-    data = request.form.to_dict()
+    data = request.args.to_dict()
     # sign 不能参与签名验证
     signature = data.pop("sign")
 
@@ -61,10 +70,22 @@ def alipayresult():
 
     # verify
     success = alipay.verify(data, signature)
+    if success:
+        result = 1
+        return render_template('result.html', result=result)
+
+# 支付宝异步通知接口
+@daoinkpay.route('/alipayresult2', methods=['POST'])
+def alipayresult2():
+    trade_out_id = request.form.get("out_trade_no")
+    data = request.form.to_dict()
+    signature = data.pop("sign")
+
+    success = alipay.verify(data, signature)
     if success and data["trade_status"] in ("TRADE_SUCCESS", "TRADE_FINISHED" ):
         result_order = Order.query.filter(Order.File_Dir == trade_out_id).first()
         result_order.Print_Status = 1
         db.session.add(result_order)
         db.session.commit()
         result = 1
-        return render_template('result.html', result=result)
+        return "success"
